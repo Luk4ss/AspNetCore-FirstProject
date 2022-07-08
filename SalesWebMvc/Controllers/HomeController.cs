ac700ae0709e167793a88bbb68ad890fd16f89ce
lucas <lucasregoaraujo@gmail.com>
2022-07-08 14:46:34 -0300

    First controller and Razor tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C# Course";
+            ViewData["Desenvolvedor"] = "Lucas Rêgo Araújo";
+            ViewData["Email"] = "lucas@gmail.com";
 
             return View();
         }
